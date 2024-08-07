28ebf4b8047cae734bc405e9faa587d51b5ba740
Sasha Limarev <slimarev@outlook.com>
2024-08-07 17:24:43 +0300

    added sample log

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,90 +2,94 @@ import { createAsync } from "@solidjs/router";
 import { createMemo, createResource, For, Suspense } from "solid-js";
 
 type Language = {
-  name: string,
-  static: boolean
-}
+  name: string;
+  static: boolean;
+};
 
 const getLanguages = async () => {
-  "use server"
+  "use server";
 
-  await new Promise(res => setTimeout(res, 3000))
+  console.log("zzz getting languages");
+
+  await new Promise((res) => setTimeout(res, 3000));
 
   return [
     {
       name: "go",
-      static: true
+      static: true,
     },
     {
       name: "python",
-      static: false
+      static: false,
     },
     {
       name: "javascript",
-      static: false
+      static: false,
     },
     {
       name: "C#",
-      static: true
-    }
-  ] satisfies Language[]
-}
+      static: true,
+    },
+  ] satisfies Language[];
+};
 
 type Framework = {
-  name: string,
-  authoringFormat: "template" | "jsx"
-}
+  name: string;
+  authoringFormat: "template" | "jsx";
+};
 
 const getFrameworks = async () => {
-  await new Promise(res => setTimeout(res, 5000))
+  await new Promise((res) => setTimeout(res, 5000));
 
   return [
     {
       name: "Angular",
-      authoringFormat: "template"
+      authoringFormat: "template",
     },
     {
       name: "SolidJS",
-      authoringFormat: "jsx"
+      authoringFormat: "jsx",
     },
     {
       name: "vue",
-      authoringFormat: "template"
+      authoringFormat: "template",
     },
     {
       name: "React",
-      authoringFormat: "jsx"
-    }
-  ] satisfies Framework[]
-}
+      authoringFormat: "jsx",
+    },
+  ] satisfies Framework[];
+};
 
 function Frameworks() {
-  const frameworks = createAsync(() => getFrameworks())
+  const frameworks = createAsync(() => getFrameworks());
 
   return (
     <>
       <Suspense fallback={"Inner loading..."}>
         <For each={frameworks()}>
-          {framework => <div>{framework.name}{' '}{framework.authoringFormat}</div>}
+          {(framework) => (
+            <div>
+              {framework.name} {framework.authoringFormat}
+            </div>
+          )}
         </For>
       </Suspense>
     </>
-  )
+  );
 }
 
 export default function Home() {
   const languages = createAsync(() => getLanguages());
   const numOfLangs = createMemo(() => {
-    return languages()?.length
-  })
+    return languages()?.length;
+  });
 
   return (
     <>
       <div>
-        There are {numOfLangs()} languages 
-        <For each={languages()}>
-          {lang => <div>{lang.name}</div>}
-        </For>
+        There are {numOfLangs()} languages
+        <For each={languages()}>{(lang) => <div>{lang.name}</div>}</For>
       </div>
       <div>
         <Frameworks />
