15e0beabc8938ce36df19f8a1eca1e6bee5a40ba
Sahsa Limarev <50263255+slimarev92@users.noreply.github.com>
2024-09-08 11:07:49 +0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,99 +1,101 @@
-import { createAsync } from "@solidjs/router";
-import { createMemo, createResource, For, Suspense } from "solid-js";
+import { createAsync } from '@solidjs/router';
+import { createMemo, For, Suspense } from 'solid-js';
 
 type Language = {
-  name: string;
-  static: boolean;
+    name: string;
+    static: boolean;
 };
 
 const getLanguages = async () => {
-  "use server";
+    'use server';
 
-  console.log("zzz getting languages");
+    console.log('zzz getting languages');
 
-  await new Promise((res) => setTimeout(res, 3000));
+    await new Promise((res) => setTimeout(res, 3000));
 
-  return [
-    {
-      name: "go",
-      static: true,
-    },
-    {
-      name: "python",
-      static: false,
-    },
-    {
-      name: "javascript",
-      static: false,
-    },
-    {
-      name: "C#",
-      static: true,
-    },
-  ] satisfies Language[];
+    return [
+        {
+            name: 'go',
+            static: true,
+        },
+        {
+            name: 'python',
+            static: false,
+        },
+        {
+            name: 'javascript',
+            static: false,
+        },
+        {
+            name: 'C#',
+            static: true,
+        },
+    ] satisfies Language[];
 };
 
 type Framework = {
-  name: string;
-  authoringFormat: "template" | "jsx";
+    name: string;
+    authoringFormat: 'template' | 'jsx';
 };
 
 const getFrameworks = async () => {
-  await new Promise((res) => setTimeout(res, 5000));
+    await new Promise((res) => setTimeout(res, 5000));
 
-  return [
-    {
-      name: "Angular",
-      authoringFormat: "template",
-    },
-    {
-      name: "SolidJS",
-      authoringFormat: "jsx",
-    },
-    {
-      name: "vue",
-      authoringFormat: "template",
-    },
-    {
-      name: "React",
-      authoringFormat: "jsx",
-    },
-  ] satisfies Framework[];
+    return [
+        {
+            name: 'Angular',
+            authoringFormat: 'template',
+        },
+        {
+            name: 'SolidJS',
+            authoringFormat: 'jsx',
+        },
+        {
+            name: 'vue',
+            authoringFormat: 'template',
+        },
+        {
+            name: 'React',
+            authoringFormat: 'jsx',
+        },
+    ] satisfies Framework[];
 };
 
 function Frameworks() {
-  const frameworks = createAsync(() => getFrameworks());
+    const frameworks = createAsync(() => getFrameworks());
 
-  return (
-    <>
-      <Suspense fallback={"Inner loading..."}>
-        <For each={frameworks()}>
-          {(framework) => (
-            <div>
-              {framework.name} {framework.authoringFormat}
-            </div>
-          )}
-        </For>
-      </Suspense>
-    </>
-  );
+    return (
+        <>
+            <hr />
+            These are the frameworks:
+            <For each={frameworks()}>
+                {(framework) => (
+                    <div>
+                        {framework.name} {framework.authoringFormat}
+                    </div>
+                )}
+            </For>
+        </>
+    );
 }
 
 export default function Home() {
-  const languages = createAsync(() => getLanguages());
-  const numOfLangs = createMemo(() => {
-    return languages()?.length;
-  });
+    const languages = createAsync(() => getLanguages());
+    const numOfLangs = createMemo(() => {
+        return languages()?.length;
+    });
 
-  return (
-    <>
-      <div>
-        There are {numOfLangs()} languages
-        <For each={languages()}>{(lang) => <div>{lang.name}</div>}</For>
-      </div>
-      <div>
-        <Frameworks />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                There are {numOfLangs()} languages
+                <For each={languages()}>{(lang) => <div>{lang.name}</div>}</For>
+            </div>
+            <div>
+                <Suspense fallback={'Inner loading...'}>
+                    <Frameworks />
+                </Suspense>
+            </div>
+        </>
+    );
 }
